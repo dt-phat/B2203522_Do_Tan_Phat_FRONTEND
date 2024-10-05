d9e849a9b8cdfe341988e6d083e5bd81c9e0d3bc
dt-phat <phatb2203522@student.ctu.edu.vn>
2024-10-06 01:37:02 +0700

    Bo sung serives

---
+++ src/services/api.service.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const commonConfig = {
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+};
+
+export default (baseURL) => {
+    return axios.create({
+        baseURL,
+        ...commonConfig,
+    });
+};
+
+
